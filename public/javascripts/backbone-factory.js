482f3b8325b5e9ac69a84a4c34c4038776718ece
Prateek Dayal <prateek@muziboo.com>
2011-06-05 13:16:25 +0530

    sequences and some other goodies

--- public/javascripts/backbone-factory.js
+++ public/javascripts/backbone-factory.js
@@ -5,20 +5,33 @@
   window.BackboneFactory = {
 
     factories: {},
+    sequences: {},
 
     define: function(factory_name, klass, defaults){
-      console.log('define');
-      console.log(klass);
-      this.factories[factory_name] = {};
-      this.factories[factory_name]['klass'] = klass;
-      this.factories[factory_name]['defaults'] = defaults;
+      //this.factories[factory_name] = {};
+      this.factories[factory_name] = function(){
+        return new klass(defaults);
+      };
+      //this.factories[factory_name]['defaults'] = defaults;
     },
 
     create: function(factory_name){
-              console.log('create');
-              console.log(this.factories[factory_name]['klass']);
-      return (new (this.factories[factory_name]['klass']));        
-    }
+      if(this.factories[factory_name] === undefined){
+        throw "Factory with name " + factory_name + " does not exist";
+      }
+      return (this.factories[factory_name].call());        
+    },
 
+    define_sequence: function(sequence_name, callback){
+      this.sequences[sequence_name] = {}
+      this.sequences[sequence_name]['counter'] = 0;
+      this.sequences[sequence_name]['callback'] = callback; 
+    },
+
+    next: function(sequence_name){
+      console.log(this.sequences[sequence_name]['counter']);
+      this.sequences[sequence_name]['counter'] += 1;
+      return this.sequences[sequence_name]['callback'].apply(null, [this.sequences[sequence_name]['counter']]); //= callback; 
+    }
   }
 })();
--- spec/javascripts/BackboneFactorySpec.js
+++ spec/javascripts/BackboneFactorySpec.js
@@ -1,22 +1,50 @@
 describe("Backbone Factory", function() {
 
-  beforeEach(function() {
-    var postFactory = BackboneFactory.define('post', Post);
-    var userFactory = BackboneFactory.define('user', User);
+  describe("Defining and using Sequences", function(){
 
-    this.postObject = BackboneFactory.create('post');
-    this.userObject = BackboneFactory.create('user');
+    beforeEach(function() {
+      var emailSequence = BackboneFactory.define_sequence('email', function(n){
+        return "person"+n+"@example.com"; 
+      });
+    });
+
+    it("should increment the sequence on successive calls", function(){
+      expect(BackboneFactory.next('email')).toBe('person1@example.com');
+      expect(BackboneFactory.next('email')).toBe('person2@example.com');
+    });
+
   });
+
+  describe("Defining and using Factories", function(){
+
+    beforeEach(function() {
+      var postFactory = BackboneFactory.define('post', Post);
+      var userFactory = BackboneFactory.define('user', User, {name : 'Backbone User'});
+
+      this.postObject = BackboneFactory.create('post');
+      this.userObject = BackboneFactory.create('user');
+    });
+    
+
+    it("return an instance of the Backbone Object requested", function() {
+      expect(this.postObject instanceof Post).toBeTruthy();
+      expect(this.userObject instanceof User).toBeTruthy();
+    });
+          
+    // Not sure if this test is needed. But what the hell!
+    it("should preserve the defaults if not overriden", function() {
+      expect(this.postObject.get('title')).toBe('Default Title');
+    });
+
+    it("should throw an error if you try to use an undefined factory", function() {
+      expect(function(){BackboneFactory.create('undefined_factory')}).toThrow("Factory with name undefined_factory does not exist");
+    });
+
+    it("should use the defaults supplied when creating objects", function() {
+      expect(this.userObject.get('name')).toBe('Backbone User');
+            
+    });
+  });  
   
-
-  it("return an instance of the Backbone Object requested", function() {
-    expect(this.postObject instanceof Post).toBeTruthy();
-    expect(this.userObject instanceof User).toBeTruthy();
-  });
-        
-  // Not sure if this test is needed. But what the hell!
-  it("should preserve the defaults if not overriden", function() {
-    expect(this.postObject.get('title')).toBe('Default Title');
-  });
 });        
 
